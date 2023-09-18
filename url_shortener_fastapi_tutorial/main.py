febc699f1155a418ecf460e00ef73af0fed39d98
stormcow <janek237krokodyl@gmail.com>
2023-09-18 13:42:07 +0200

    pub

---
+++ url_shortener_fastapi_tutorial/main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI, HTTPException, Depends, Request
+from sqlalchemy import schema
+from fastapi.responses import RedirectResponse
+from starlette.datastructures import URL
+
+# import schemas
+import models
+import schemas
+import validators
+import crud
+from config import get_settings
+
+from database import SesssionLocal, engine
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SesssionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+@app.get("/")
+def read_root():
+    return "Hello there"
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        crud.update_db_clicks(db=db, db_url=db_url)
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
+    return get_admin_info(db_url)
+
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        db_url.url = db_url.key
+        db_url.admin_url = db_url.secret_key
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
+@app.delete("/admin/{secret_key}")
+def delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
+        message = f"Successfully deleted shoretened URL for {db_url.target_url}"
+        return {"detail": message}
+    else:
+        raise_not_found(request)
---
+++ url_shortener_fastapi_tutorial/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class URLBase(BaseModel):
+    target_url: str
+    custom_url: str = "none"
+
+
+class URL(URLBase):
+    is_active: bool
+    click: int
+
+    class Config:
+        from_attributes = True
+
+
+class URLInfo(URL):
+    url: str
+    admin_url: str
